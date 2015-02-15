c619a3b3ad2e0517e0f2db04cff318b7f9a9d875
mayk93 <mihai.mandrescu@gmail.com>
2015-02-15 15:35:54 +0200

    Scene Loader
    Scene Loader for Tamagotchi game.

---
+++ SeceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class SeceneLoader : MonoBehaviour {
+
+	public float lastTimePlayed = 0f;
+	public float currentTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LoadLevel ()
+	{
+		try
+		{
+			LoadSavedGame();
+		}
+		catch
+		{
+			Application.LoadLevel("NewGame");
+		}
+	}
+
+	void LoadSavedGame ()
+	{
+	}
+}
